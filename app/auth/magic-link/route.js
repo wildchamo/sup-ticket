a458e8bd537a0642d5cc519da95dd3a3f72d72e2
Jose Luis <wildchamo@gmail.com>
2025-04-24 16:53:00 -0500

    sending custom email for recovery n magic login

--- app/auth/magic-link/route.js
+++ app/auth/magic-link/route.js
@@ -43,15 +43,22 @@ const type = searchParams.get("type");
     port: 54325,
   });
 
-  await transporter.sendMail({
-    from: "Your Company <your@mail.whatever>",
-    to: email,
-    subject: "Magic Link",
-    html: `
+  const subject = type === "recovery" ? "Password Recovery" : "Magic Link";
+
+  const emailBody = type === "recovery" ? `
+    <h1>Hi there, this is a password recovery email!</h1>
+    <p>Click <a href="${constructedLink.toString()}">here</a> to recover.</p>
+    ` : `
     <h1>Hi there, this is a custom magic link email!</h1>
     <p>Click <a href="${constructedLink.toString()}">here</a> to log 
       in.</p>
-    `,
+    `;
+
+  await transporter.sendMail({
+    from: "Your Company <your@mail.whatever>",
+    to: email,
+    subject: subject,
+    html: emailBody,
   });
 
   const thanksUrl = new URL("/magic-thanks", request.url);
