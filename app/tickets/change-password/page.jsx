773122df600df23d6f78d69b2dbd34bfd71a4b14
Jose Luis <wildchamo@gmail.com>
2025-04-25 15:59:19 -0500

    auth working

--- app/tickets/change-password/page.jsx
+++ app/tickets/change-password/page.jsx
@@ -1,3 +1,42 @@
-export default function ChangePassword() {
-  return <h2>Change Password</h2>;
+"use client";
+
+import { createSupabaseBrowserClient } from "@/supabase-utils/browser-client";
+import { useRef } from "react";
+
+export default function ChangePasswordPage() {
+  const passwordRef = useRef(null);
+  const supabase = createSupabaseBrowserClient();
+
+  return (
+    <form
+      onSubmit={(event) => {
+        event.preventDefault();
+        const value = (passwordRef.current.value || "").trim();
+
+        supabase.auth.updateUser({ password: value }).then((result) => {
+          if (result.error) {
+            alert(result.error.message);
+          } else {
+            alert("Password updated!");
+            passwordRef.current.value = "";
+          }
+        });
+      }}
+    >
+      <fieldset>
+        <label htmlFor="password">
+          New Password{" "}
+          <input
+            ref={passwordRef}
+            name="password"
+            type="password"
+            id="password"
+            required
+          />
+        </label>
+      </fieldset>
+
+      <button type="submit">Reset Password</button>
+    </form>
+  );
 }
