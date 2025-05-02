a033e001f1fa0c78a5f816ff993d55a298cb391b
Jose Luis <wildchamo@gmail.com>
2025-05-01 22:13:01 -0500

    register route grabbing emailhost

--- app/[tenant]/auth/register/route.js
+++ app/[tenant]/auth/register/route.js
@@ -1,8 +1,24 @@
 export async function POST(request, { params }) {
+  const isNonEmptyString = (value) =>
+    typeof value === "string" && value.trim().length > 0;
+  const emailRegex = /^\S+@\S+$/; // simple front@back regex
+
   const formData = await request.formData();
   const name = formData.get("name");
   const email = formData.get("email");
   const password = formData.get("password");
   const tenant = params.tenant;
-  return Response.json({ email, password, tenant });
+
+  if (
+    !isNonEmptyString(name) ||
+    !isNonEmptyString(email) ||
+    !emailRegex.test(email) ||
+    !isNonEmptyString(password)
+  ) {
+    return NextResponse.redirect(buildUrl("/error", tenant, request), 302);
+  }
+
+  const [, emailHost] = email.split("@");
+
+  return Response.json({ email, password, tenant, emailHost });
 }
