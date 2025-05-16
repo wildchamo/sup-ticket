9c25f477d92274f5e731641d493d3aa19d2240c8
Jose Luis <wildchamo@gmail.com>
2025-05-16 12:55:04 -0500

    suspense for ticket list

--- app/[tenant]/tickets/page.jsx
+++ app/[tenant]/tickets/page.jsx
@@ -1,11 +1,18 @@
+import { Suspense } from "react";
 import { TicketList } from "./TicketList";
+export const dynamic = "force-dynamic";
 
 export default async function TicketPage({ params, searchParams }) {
   const { tenant } = await params;
   return (
     <div>
       <h1>Ticket List</h1>
-      <TicketList tenant={tenant} searchParams={searchParams}></TicketList>
+      <Suspense
+        fallback={<div aria-busy="true">Loading tickets...</div>}
+        key={JSON.stringify(searchParams)}
+      >
+        <TicketList tenant={tenant} searchParams={searchParams}></TicketList>
+      </Suspense>
     </div>
   );
 }
